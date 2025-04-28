f841c2d03f22cc59eb81cd5f454550c594f1d7d4
RePuLs05 <vaja.zoidze.05@gmail.com>
2025-04-29 00:39:16 +0400

    feat: create brends scroll animation

---
+++ js/mainPage.js
@@ -0,0 +1,42 @@
+// Burger Menu Functionality
+function openMenu() {
+  document.getElementById("sideMenu").classList.add("open");
+}
+
+function closeMenu() {
+  document.getElementById("sideMenu").classList.remove("open");
+}
+
+function toggleDropdown() {
+  const dropdown = document.getElementById("dropdownMenu");
+  const toggle = document.querySelector(".dropdown-toggle");
+  dropdown.classList.toggle("open");
+  toggle.classList.toggle("open");
+}
+
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
